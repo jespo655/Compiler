a4596ddba5fb5ff52e5ccf307c4980e6e1670a90
jespo655 <jespo655@student.liu.se>
2016-06-22 07:26:56 +0200

    added some backend notes, and beginning of basic library. Added copy to path for output .exe

---
+++ Code/jm/basic.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+
+void c_include_test() {
+    printf("hej\n");
+}
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    c_include_test();
+    return 0;
+}
--- Code/make.bat
+++ Code/make.bat
@@ -1,2 +1,7 @@
 @echo off
 g++ -std=c++11 *.cpp abstx/*.cpp utilities/*.cpp compile_time/*.cpp
+
+:: /B indicates a binary file
+:: /Y supresses prompting to confirm you want to overwrite an existing destination file (seems to not do anything)
+:: copy /B /Y "a.exe" "D:/PATH/jm.exe"
+copy /B "a.exe" "D:/PATH/jm.exe"
