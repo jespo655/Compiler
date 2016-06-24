4dc5c847fda210728172b46aa1fd3785ce738da1
Jesper Post <jespo655@student.liu.se>
2016-06-24 08:16:34 +0200

    updated make script

--- Code/make.bat
+++ Code/make.bat
@@ -1,7 +1,9 @@
 @echo off
-g++ -std=c++11 *.cpp abstx/*.cpp utilities/*.cpp compile_time/*.cpp
+g++ -std=c++11 *.cpp abstx/*.cpp utilities/*.cpp compile_time/*.cpp %*
 
 :: /B indicates a binary file
 :: /Y supresses prompting to confirm you want to overwrite an existing destination file (seems to not do anything)
-:: copy /B /Y "a.exe" "D:/PATH/jm.exe"
-copy /B "a.exe" "D:/PATH/jm.exe"
+:: /Q : Suppresses the display of xcopy messages.
+:: /R : Copies read-only files.
+xcopy /B /Q /Y "a.exe" "C:/PATH/jm.exe"
+:: xcopy /B /Q /Y "a.exe" "D:/PATH/jm.exe"
