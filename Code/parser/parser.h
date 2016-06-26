b9cb5a52f67a7ada6134e28a26d1a9e8aa49eaf1
jespo655 <jespo655@student.liu.se>
2016-06-26 22:25:04 +0200

    updated todos. Found a potential problem with the current parser layout

---
+++ Code/parser/parser.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../abstx/abstx.h"
+#include "../abstx/workspace.h"
+#include "token.h"
+
+
+
+/*
+The parser builds an abstract syntax tree from a set of tokens
+*/
+
+
+
+/*
+Problem:
+
+for incremental compilation, each statement has to know about its own list of tokens (unless finalized)
+
+
+
+
+*/
