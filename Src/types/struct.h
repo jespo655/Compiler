e55935aef04e3415449b59790d8f7bf45efc03db
Jesper Post <jespo655@student.liu.se>
2017-12-04 19:51:09 +0100

    nice bus ride working on structs

--- Src/types/struct.h
+++ Src/types/struct.h
@@ -139,8 +139,11 @@ struct CB_Struct {
     static CB_Type type;
     CB_Sharing_pointer<CB_Struct_type> struct_type;
 
-    uint8_t* data; // c++ special case: everything is stored on the heap in an array of bytes
+    typedef uint8_t byte;
+    byte* data; // c++ special case: everything is stored on the heap in an array of bytes (it will be on the stack in C)
 
+    CB_Struct() {}
+    ~CB_Struct() { free(data); }
 };
 
 
@@ -149,6 +152,113 @@ struct CB_Struct {
 
 
 
+
+
+
+/*
+    Struct_instance(const Struct_type& struct_type) : struct_type{struct_type} { init_members(); };
+
+    // copy and move constructors, destructor
+    Struct_instance(const Struct_instance& si) : struct_type{si.struct_type} { *this = si; }
+    Struct_instance(Struct_instance&& si) : struct_type{std::move(si.struct_type)} { *this = std::move(si); }
+    Struct_instance& operator=(const Struct_instance& si) { ASSERT(struct_type == si.struct_type); values = si.values; }
+    Struct_instance& operator=(Struct_instance&& si) { ASSERT(struct_type == si.struct_type); values = std::move(si.values); }
+    ~Struct_instance() {}
+
+    int get_CB_size() { ASSERT(false, "NYI"); } // TODO: implement
+
+    bool is_same_type(const CB_Any& any, const CB_Type& type) const {
+        // compare the type of an any with a CB_Type
+        // if the any is of a struct type, compare its value's struct_type
+        // else, compare its v_type
+
+        if (any.v_type == Struct_instance::type) {
+            // the value is a struct, so we should compare struct types
+            return (CB_Type)any.value<Struct_instance>().struct_type == type;
+        }
+        else return any.v_type == type;
+    }
+
+    std::string toS() const {
+        ASSERT(struct_type.metadata != nullptr);
+        ASSERT(values.size == struct_type.metadata->members.size);
+        std::ostringstream oss;
+
+        const std::string& type_name = struct_type.typenames[struct_type.uid];
+        if (type_name == "") oss << "instance of anonymous struct_type("+std::to_string(struct_type.uid)+")";
+        else oss << type_name;
+
+        oss << "(";
+        for (int i = 0; i < values.size; ++i) {
+            if (i > 0) oss << " ";
+            Struct_member& member_metadata = struct_type.metadata->members[i];
+            oss << member_metadata.id.toS() << ":";
+            oss << member_metadata.type.toS() << "=";
+            if (member_metadata.explicit_uninitialized) oss << "---";
+            else {
+                // get the current value
+                ASSERT(is_same_type(values[i], member_metadata.type));
+                oss << values[i].toS();
+            }
+
+            oss << ";";
+        }
+        oss << ")";
+        return oss.str();
+    }
+
+    void init_members() {
+        ASSERT(struct_type.metadata != nullptr);
+        values.clear();
+        for (auto& member : struct_type.metadata->members) {
+            values.add(member.default_value);
+        }
+    }
+
+    CB_Any& member(const CB_String& name) {
+        ASSERT(struct_type.metadata != nullptr);
+        ASSERT(values.size == struct_type.metadata->members.size);
+
+        CB_Sharing_pointer<Struct_instance> used_struct_with_member = nullptr;
+
+        for (int i = 0; i <  values.size; ++i) {
+            Struct_member& member_metadata = struct_type.metadata->members[i];
+            if (member_metadata.id == name) {
+                ASSERT(is_same_type(values[i], member_metadata.type));
+                return values[i];
+            }
+
+            // If a struct member is marked with the 'using' keyword, its members can also be reached.
+            if (member_metadata.is_using && values[i].v_type == Struct_instance::type) {
+                if (values[i].value<Struct_instance>().has_member(name)) {
+                    ASSERT(used_struct_with_member == nullptr, "Ambiguous struct member: "+name.toS());
+                    used_struct_with_member = &values[i].value<Struct_instance>();
+                }
+            }
+        }
+
+        if (used_struct_with_member != nullptr) return used_struct_with_member->member(name);
+
+        ASSERT(false, "Cannot find member "+name.toS()+" in "+toS()); // log_error?
+    }
+
+    bool has_member(const CB_String& name) const {
+        ASSERT(struct_type.metadata != nullptr);
+        ASSERT(values.size == struct_type.metadata->members.size);
+        for (int i = 0; i <  values.size; ++i) {
+            const Struct_member& member_metadata = struct_type.metadata->members[i];
+            if (member_metadata.id == name) return true;
+
+            // If a struct member is marked with the 'using' keyword, its members can also be reached.
+            if (member_metadata.is_using && values[i].v_type == Struct_instance::type) {
+                auto member = values[i].value<Struct_instance>();
+                if (member.has_member(name)) return true;
+            }
+        }
+        return false;
+    }
+
+*/
 
 
 
