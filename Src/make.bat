e11c4c6eeabeda88f296542e9191c4b87b54e06c
EUROPE\lkpjpost <jesper.post@flex.com>
2018-02-06 13:04:24 +0100

    added sublime project

--- Src/make.bat
+++ Src/make.bat
@@ -3,15 +3,16 @@
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto 64BIT
 
 :32BIT
+echo Compiling cube_32...
 g++ -std=gnu++11 *.cpp lexer/*.cpp utilities/*.cpp types/*.cpp runtime_dll/*.cpp runtime_dll/dyncall/lib32/libdyncall_s.lib -o cube.exe
 goto END
 
 :64BIT
+echo Compiling cube_64...
 g++ -std=gnu++11 *.cpp lexer/*.cpp utilities/*.cpp types/*.cpp runtime_dll/*.cpp runtime_dll/dyncall/lib64/libdyncall_s.lib -o cube.exe
 :: parser/*.cpp
 
 :END
-
 
 :: /B indicates a binary file
 :: /Y supresses prompting to confirm you want to overwrite an existing destination file (seems to not do anything)
