176bfb088d06bbfe614e69a0ede4f2195ebe7a31
jespo655 <jespo655@student.liu.se>
2018-02-10 19:25:12 +0100

    updated make script to use variables

--- Src/make.bat
+++ Src/make.bat
@@ -1,15 +1,21 @@
 @echo off
 
+set INCLUDE_PATHS=
+set SRC_FILES=*.cpp lexer/*.cpp utilities/*.cpp types/*.cpp runtime_dll/*.cpp
+set LIBS32=runtime_dll/dyncall/lib32/libdyncall_s.lib
+set LIBS64=runtime_dll/dyncall/lib64/libdyncall_s.lib
+set OUTPUT_NAME=cube.exe
+
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto 64BIT
 
 :32BIT
 echo Compiling cube_32...
-g++ -std=gnu++11 *.cpp lexer/*.cpp utilities/*.cpp types/*.cpp runtime_dll/*.cpp runtime_dll/dyncall/lib32/libdyncall_s.lib -o cube.exe
+g++ -std=gnu++11 %INCLUDE_PATHS% %SRC_FILES% %LIBS32% -o %OUTPUT_NAME%
 goto END
 
 :64BIT
 echo Compiling cube_64...
-g++ -std=gnu++11 *.cpp lexer/*.cpp utilities/*.cpp types/*.cpp runtime_dll/*.cpp runtime_dll/dyncall/lib64/libdyncall_s.lib -o cube.exe
+g++ -std=gnu++11 %INCLUDE_PATHS% %SRC_FILES% %LIBS64% -o %OUTPUT_NAME%
 :: parser/*.cpp
 
 :END
