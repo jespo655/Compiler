43c1a849ae846af99d0d2a460f86fec880f4b502
Jesper Post <Jesper.Post@flex.com>
2018-04-30 01:02:13 +0200

    moved pointers.h and sequence.h to utilities; rewrote them for generic use instead of CB use

---
+++ Src/utilities/pointers.h
@@ -0,0 +1,238 @@
+#pragma once
+
+#include "assert.h"
+
+#include <string>
+#include <sstream>
+#include <type_traits>
+
+using std::nullptr_t;
+
+/*
+There are two types of pointers: owning and sharing.
+An owning pointer owns an object on the heap, and will delete it when itself is deleted.
+A sharing pointer points to an object owned by something else, and will never delete it.
+
+Syntax:
+op : *!T = alloc(t); // t is copied to the heap and that copy is now owned by op. // TODO: find better syntax for allocation
+sp : *T = op; // sp now points to the object owned by op.
+sp2 : *T = sp; // sp and sp2 now points to the same object.
+op2 : *!T = op; // op2 grabs the object from op and is now the owner of the object. op is now null.
+// op : *!T = sp; // illegal, sp doesn't own the object so op can't grab it
+
+// sharing pointers are automatically casted to their base type.
+t1 : T;
+sp1 : *T = t1; // sp1 now points to t1
+t2 : T = sp1; // t2 is now a copy of t1
+sp2 : *T = sp1; // sp2 now point to the same object as sp1 points 2
+
+*sp1 = t2; // the value that sp1 points to is now a copy of t2
+sp1 = t2; // sp1 now points to t2 (implicit "adress of")
+
+// owning pointers are automatically casted to their base type, but not for assignment
+op1 : *!T = alloc(T); // has to use alloc here to allocate on the heap
+t2 : T = op1; // t is now a copy of the data that op1 points to (owns)
+op2 : *!T = op1; // grabs the object from op and is now the owner of the object. op is now null.
+sp : *T = op1; // sp now points at the object that op1 is pointing to.
+spp : **T = op1; // spp now points to op1 (pointer to a pointer to a T)
+spp : **!T = op1; // spp now points to op1 (poiter to a owning pointer of T)
+// spp : *!*T = op1; // not allowed, spp can never own op1 (pointer that owns a pointer to a T)
+
+*op1 = t2; // the value that op1 points to is now a copy of t2
+// op1 = t2; // not allowed, since op1 can never own t1
+
+
+adress_of($T t) -> *T {
+    tp : *T = t;
+    return tp;
+}
+
+alloc($T : type)-> *!T {
+    ptr : *!T;
+    // predefined allocation of a t
+    return ptr;
+}
+
+*/
+
+
+
+/*
+owned should have deleted copy construcor and copy assignment operator.
+However, that gives lots of problems with template classes such as seq<OP> and any (assignment callbacks).
+
+
+*/
+
+template<typename T> struct owned;
+template<typename T> owned<T> alloc(const T& t);
+template<typename T> owned<T> alloc(T&& t);
+template<typename T, bool> struct deep_copy;
+
+
+
+// owning pointer - owns the object and deallocates it when done.
+template<typename T>
+struct owned {
+    T* v = nullptr;
+
+    std::string toS() const {
+        std::ostringstream oss;
+        oss << "*!(" << std::hex << v << ")";
+        return oss.str();
+    }
+
+    // default constructor
+    owned() {}
+    ~owned() {
+        if (v != nullptr) {
+            v->~T();
+            free(v);
+        }
+        v = nullptr;
+    }
+
+    T* operator->() const { ASSERT(v != nullptr); return v; }
+    T& operator*() const { ASSERT(v != nullptr); return *v; }
+
+    // copy - not allowed (makes a deep copy?). If passing a owned_ptr to a function, move constructor should be used. (In CB this will be done automatically)
+    owned& operator=(const owned& ptr)
+    {
+        if (v == nullptr) *this = std::move(ptr.deep_copy());
+        else *v = *ptr;
+    }
+    owned(const owned& ptr) { *this = ptr; }
+
+    owned& operator=(const nullptr_t& ptr) { ASSERT(ptr == nullptr); this->~owned(); return *this; }
+    owned(const nullptr_t& ptr) { *this = ptr; }
+
+    // move
+    owned& operator=(owned&& ptr) {
+        this->~owned();
+        v = ptr.v;
+        ptr.v = nullptr;
+        return *this;
+    }
+    owned(owned&& ptr) { *this = std::move(ptr); }
+
+    owned& operator=(T*&& ptr) {
+        this->~owned();
+        v = ptr;
+        ptr = nullptr;
+        return *this;
+    }
+    owned(T*&& ptr) { *this = std::move(ptr); }
+
+    owned deep_copy() const {
+        return ::deep_copy<T, std::is_copy_constructible<T>::value>()(*this);
+    }
+};
+
+
+template<typename T>
+owned<T> alloc(const T& t) {
+    owned<T> ptr;
+    ptr.v = (T*)malloc(sizeof(T));
+    ASSERT(ptr.v != nullptr);
+    new (ptr.v) T(t);
+    return ptr;
+}
+
+template<typename T>
+owned<T> alloc(T&& t) {
+    owned<T> ptr;
+    ptr.v = (T*)malloc(sizeof(T));
+    new (ptr.v) T(std::move(t));
+    return ptr;
+}
+
+template<typename T> struct deep_copy<T,true> { // copy constructible
+    owned<T> operator()(const owned<T>& ptr) {
+        if (ptr == nullptr) return nullptr;
+        else return alloc<T>(*ptr);
+    }
+};
+
+template<typename T> struct deep_copy<T,false> { // non-copy constructible - deep copies are not allowed.
+    owned<T> operator()(const owned<T>& ptr) { ASSERT(false); return nullptr; }
+};
+
+
+// sharing pointer - never deallocates the object
+// shares happily with other sharing pointers
+// can not be created from an object, only from other pointers
+template<typename T> // T is a CB type
+struct shared {
+    T* v = nullptr;
+
+    std::string toS() const {
+        std::ostringstream oss;
+        oss << "*(" << std::hex << v << ")";
+        return oss.str();
+    }
+
+    // default constructor
+    shared() {}
+    ~shared() { v = nullptr; }
+
+    T* operator->() const { ASSERT(v != nullptr); return v; }
+    T& operator*() const { ASSERT(v != nullptr); return *v; }
+
+    // copy
+    shared& operator=(const shared& ptr) { v = ptr.v; return *this; }
+    shared(const shared& ptr) { *this = ptr; }
+
+    shared& operator=(const owned<T>& ptr) { v = ptr.v; return *this; }
+    shared(const owned<T>& ptr) { *this = ptr; }
+
+    shared& operator=(T* ptr) { v = ptr; return *this; }
+    shared(T* ptr) { *this = ptr; }
+
+    shared& operator=(const nullptr_t& ptr) { ASSERT(ptr == nullptr); v = ptr; return *this; }
+    shared(const nullptr_t& ptr) { *this = ptr; }
+};
+
+// comparison betweens different kinds of pointers
+template<typename T> bool operator==(const shared<T>& lhs, const shared<T>& rhs) { return lhs.v == rhs.v; }
+template<typename T> bool operator==(const owned<T>& lhs, const owned<T>& rhs) { return lhs.v == rhs.v; }
+template<typename T> bool operator==(const shared<T>& lhs, const owned<T>& rhs) { return lhs.v == rhs.v; }
+template<typename T> bool operator==(const owned<T>& lhs, const shared<T>& rhs) { return lhs.v == rhs.v; }
+template<typename T> bool operator==(const shared<T>& lhs, const T* rhs) { return lhs.v == rhs; }
+template<typename T> bool operator==(const T* lhs, const shared<T>& rhs) { return lhs == rhs.v; }
+template<typename T> bool operator==(const owned<T>& lhs, const T* rhs) { return lhs.v == rhs; }
+template<typename T> bool operator==(const T* lhs, const owned<T>& rhs) { return lhs == rhs.v; }
+template<typename T> bool operator==(const shared<T>& lhs, const nullptr_t rhs) { return lhs.v == rhs; }
+template<typename T> bool operator==(const nullptr_t lhs, const shared<T>& rhs) { return lhs == rhs.v; }
+template<typename T> bool operator==(const owned<T>& lhs, const nullptr_t rhs) { return lhs.v == rhs; }
+template<typename T> bool operator==(const nullptr_t lhs, const owned<T>& rhs) { return lhs == rhs.v; }
+
+template<typename T> bool operator!=(const shared<T>& lhs, const shared<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const owned<T>& lhs, const owned<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const shared<T>& lhs, const owned<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const owned<T>& lhs, const shared<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const shared<T>& lhs, const T* rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const T* lhs, const shared<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const owned<T>& lhs, const T* rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const T* lhs, const owned<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const shared<T>& lhs, const nullptr_t rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const nullptr_t lhs, const shared<T>& rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const owned<T>& lhs, const nullptr_t rhs) { return !(lhs == rhs); }
+template<typename T> bool operator!=(const nullptr_t lhs, const owned<T>& rhs) { return !(lhs == rhs); }
+
+template<typename T, typename T2>
+shared<T> dynamic_pointer_cast(const owned<T2>& ptr) {
+    return shared<T>(dynamic_cast<T*>(ptr.v));
+}
+template<typename T, typename T2>
+shared<T> dynamic_pointer_cast(const shared<T2>& ptr) {
+    return shared<T>(dynamic_cast<T*>(ptr.v));
+}
+
+template<typename T, typename T2>
+owned<T> owning_pointer_cast(owned<T2>&& ptr) {
+    auto p = owned<T>(dynamic_cast<T*>(ptr.v));
+    if (p.v != nullptr) ptr.v = nullptr;
+    return p;
+}
+
+
---
+++ Src/utilities/sequence.h
@@ -0,0 +1,306 @@
+#pragma once
+
+#include "assert.h"
+
+#include <string>
+#include <sstream>
+#include <cstring>
+
+/*
+Dynamic sequence - stores the elements on the heap
+
+Syntax:
+a : T[] = [T, size=N: t1, t2, t3]; // T inferred from members, N evaluated compile time, must be a positive integer.
+a : T[] = [T: t1, t2, t3]; // N inferred by the number of arguments
+a : T[] = [size=N: t1, t2, t3]; // T inferred from the type of the members
+a : T[] = [t1, t2, t3]; // T and N inferred
+*/
+
+const int DEFAULT_CAPACITY = 16; // arbitrary power of 2
+
+template<typename T>
+struct seq {
+    uint32_t size = 0;
+    uint32_t capacity = DEFAULT_CAPACITY;
+    T* v_ptr = nullptr;
+
+    std::string toS() const {
+        std::ostringstream oss;
+        oss << "[";
+        for (uint32_t i = 0; i < size; ++i) {
+            if (i != 0) oss << ", ";
+            oss << v_ptr[i].toS();
+        }
+        oss << "]";
+        return oss.str();
+    }
+
+    seq() {
+        size = 0;
+        capacity = DEFAULT_CAPACITY;
+        v_ptr = (T*)malloc(capacity*sizeof(T));
+        // size is always 0 at init, so no need to init members
+    }
+
+    ~seq() {
+        for (uint32_t i = 0; i < size; ++i) {
+            v_ptr[i].~T();
+        }
+        free(v_ptr);
+        v_ptr = nullptr;
+        size = 0;
+        capacity = 0;
+    }
+
+    // copy
+    seq& operator=(const seq& seq) {
+        for (uint32_t i = 0; i < size; ++i) {
+            v_ptr[i].~T();
+        }
+        resize(seq.size, false);
+        for (uint32_t i = 0; i < size; ++i) {
+            new (&v_ptr[i]) T(seq.v_ptr[i]);
+        }
+        return *this;
+    }
+    seq(const seq& seq) { *this = seq; }
+
+    // move
+    seq& operator=(seq&& seq) {
+        this->~seq(); // free all members
+        size = seq.size;
+        capacity = seq.capacity;
+        v_ptr = seq.v_ptr;
+
+        seq.size = 0;
+        seq.capacity = 0;
+        seq.v_ptr = nullptr;
+        return *this;
+    }
+    seq(seq&& seq) { *this = std::move(seq); }
+
+
+    T& operator[](uint32_t index) {
+        if (index >= size) set(index, T());
+        return v_ptr[index];
+    }
+    T operator[](uint32_t index) const { return get(index); }
+
+
+    template<typename T2> operator==(const seq<T2>& seq) const { return false; }
+    bool operator==(const seq<T>& seq) const {
+        if (size != seq.size) return false;
+        for (int i = 0; i < size; ++i) {
+            if (v_ptr[i] != seq.v_ptr[i]) return false;
+        }
+        return true;
+    }
+
+    T get(uint32_t index) const {
+        if (index >= size) return T();
+        return v_ptr[index];
+    }
+
+    void set(uint32_t index, const T& t) {
+        ASSERT(v_ptr != nullptr);
+        ASSERT(capacity != 0);
+        if (index < size)
+            v_ptr[index] = t;
+        else {
+            while (index >= capacity) {
+                reallocate(2*capacity);
+            }
+            for (uint32_t i = size; i < index; ++i) {
+                new (&v_ptr[i]) T(); // fill with default values
+            }
+            new (&v_ptr[index]) T(t);
+            size = index+1;
+        }
+    }
+
+    void add(const T& t) {
+        set(size, t);
+    }
+
+    void resize(uint32_t new_size, bool init=true, T default_value=T()) {
+        if (new_size > size) {
+            if (new_size > capacity) {
+                reallocate(new_size);
+            }
+            if (init) {
+                for (uint32_t i = size; i < new_size; ++i) {
+                    new (&v_ptr[i]) T(default_value); // fill with default values
+                }
+            }
+        } else {
+            // properly delete everything outside the new size
+            for (uint32_t i = new_size; i < size; ++i) v_ptr[i].~T();
+        }
+        size = new_size;
+    }
+
+    void clear() {
+        // properly delete cleared things
+        for (uint32_t i = 0; i < size; ++i)
+            v_ptr[i].~T();
+        size = 0;
+    }
+
+    void reallocate(uint32_t capacity, bool move=true) {
+        if (this->capacity == capacity) return;
+        this->capacity = capacity;
+        T* new_ptr = (T*)malloc(capacity*sizeof(T));
+        ASSERT(new_ptr != nullptr);
+        if (capacity < size) {
+            for (uint32_t i = capacity; i < size; ++i) v_ptr[i].~T(); // properly delete the Ts we won't move
+            size = capacity;
+        }
+        if (move) memcpy(new_ptr, v_ptr, size*sizeof(T)); // move the values
+        else for (uint32_t i = 0; i < size; ++i) v_ptr[i].~T(); // properly delete all Ts we don't move
+        free(v_ptr);
+        v_ptr = new_ptr;
+    }
+
+    struct iterator {
+        uint32_t index = 0;
+        const seq& _seq;
+        iterator(const seq& _seq, uint32_t i=0) : _seq{_seq}, index{i} {}
+        const T& operator*() const { ASSERT(index < _seq.size); return _seq.v_ptr[index]; }
+        iterator& operator++() { ++index; return *this; }
+        bool operator==(const iterator& o) const { return index == o.index; }
+        bool operator!=(const iterator& o) const { return !(*this == o); }
+        bool operator<(const iterator& o) const { return index < o.index; }
+    };
+
+    iterator begin() const { return iterator(*this); }
+    iterator end() const { return iterator(*this, size); }
+};
+
+
+
+
+/*
+Static sequence - stores the elements on the stack
+
+Syntax:
+a : T[N] = [T, size=N: t1, t2, t3]; // T inferred from members, N evaluated compile time, must be a positive integer.
+a : T[N] = [T: t1, t2, t3]; // N inferred by the number of arguments
+a : T[N] = [size=N: t1, t2, t3]; // T inferred from the type of the members
+a : T[N] = [t1, t2, t3]; // T and N inferred
+*/
+
+template<typename T, uint32_t c_size>
+struct fixed_seq {
+    constexpr static uint32_t size = c_size;
+    constexpr static uint32_t capacity = c_size;
+    T v[c_size];
+
+    std::string toS() const {
+        std::ostringstream oss;
+        oss << "[size=" << size << ": ";
+        for (uint32_t i = 0; i < size; ++i) {
+            if (i != 0) oss << ", ";
+            oss << v[i].toS();
+        }
+        oss << "]";
+        return oss.str();
+    }
+
+    fixed_seq(bool init=true) {
+        if (init) {
+            for (uint32_t i = 0; i < size; ++i) {
+                new (&v[i]) T();
+            }
+        }
+    }
+    ~fixed_seq() {
+        for (uint32_t i = 0; i < size; ++i) v[i].~T();
+    }
+
+    T& operator[](uint32_t index) {
+        ASSERT(index < c_size);
+        return v[index];
+    }
+    T operator[](uint32_t index) const { return get(index); }
+
+    T get(uint32_t index) const {
+        if (index >= c_size) return T();
+        return v[index];
+    }
+
+    void set(uint32_t index, T t) {
+        if (index < c_size) {
+            v[index] = t;
+        } else {
+            // outside the array -> ignore
+        }
+    }
+
+    explicit operator seq<T>() const
+    {
+        seq<T> seq;
+        seq.reallocate(size);
+        seq.size = size;
+        memcpy(seq.v_ptr, v, size*sizeof(T));
+        return seq;
+    }
+
+    struct iterator {
+        uint32_t index = 0;
+        const fixed_seq& seq;
+        iterator(const fixed_seq& seq, uint32_t i=0) : seq{seq}, index{i} {}
+        const T& operator*() const { ASSERT(index < seq.size); return seq.v_ptr[index]; }
+        iterator& operator++() { ++index; return *this; }
+        bool operator==(const iterator& o) const { return index == o.index; }
+        bool operator!=(const iterator& o) const { return !(*this == o); }
+        bool operator<(const iterator& o) const { return index < o.index; }
+    };
+
+    iterator begin() const { return iterator(*this); }
+    iterator end() const { return iterator(*this, size); }
+};
+
+
+
+
+
+
+
+
+/*
+// Problem: Partial template of functions doesn't work @check
+
+// copy
+template<typename T>
+seq<T>& seq<T>::operator=(const seq<T>& seq) {
+    for (uint32_t i = 0; i < size; ++i) {
+        v_ptr[i].~T();
+    }
+    resize(seq.size, false);
+    for (uint32_t i = 0; i < size; ++i) {
+        new (&v_ptr[i]) T(seq.v_ptr[i]);
+    }
+    return *this;
+}
+
+#include "pointers.h"
+template<typename T, typename PT=CB_Owning_pointer<T>>
+seq<PT>& seq<PT>::operator=(const seq<PT>& seq) {
+    for (uint32_t i = 0; i < size; ++i) {
+        v_ptr[i].~T();
+    }
+    resize(seq.size, false);
+    for (uint32_t i = 0; i < size; ++i) {
+        // new (&v_ptr[i]) T(seq.v_ptr[i]);
+    }
+    return *this;
+}
+
+
+*/
+
+
+
+
+
+
