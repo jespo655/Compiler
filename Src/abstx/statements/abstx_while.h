4e0202dc97a30132c2785c0f9ff643bca03bf512
EUROPE\lkpjpost <jesper.post@flex.com>
2018-05-02 10:51:51 +0200

    added abstx_while implementation

--- Src/abstx/statements/abstx_while.h
+++ Src/abstx/statements/abstx_while.h
@@ -3,6 +3,7 @@
 #include "abstx_statement.h"
 #include "abstx_scope.h"
 #include "../expressions/value_expression.h"
+#include "../../types/cb_primitives.h"
 
 /*
 while (b) {}
@@ -29,6 +30,37 @@ struct While_statement : Statement {
         }
         else os << std::endl;
     }
+
+    Parsing_status finalize() override {
+        if (is_codegen_ready(status)) return status;
+
+        if (!is_codegen_ready(condition->finalize())) {
+            status = Parsing_status::DEPENDENCIES_NEEDED;
+            return status;
+        }
+        seq<shared<const CB_Type>> types = condition->get_type();
+        if (types.size != 1 || *types[0] != *CB_Bool::type) {
+            status = Parsing_status::TYPE_ERROR;
+            return status;
+        }
+        if (!is_codegen_ready(scope->finalize())) {
+            status = Parsing_status::DEPENDENCIES_NEEDED;
+            return status;
+        }
+        // we reached the end -> we are done
+        status = Parsing_status::FULLY_RESOLVED;
+        return status;
+    }
+
+    void generate_code(std::ostream& target) override {
+        ASSERT(is_codegen_ready(status));
+        target << "while (";
+        condition->generate_code(target);
+        target << ") ";
+        scope->generate_code(target);
+        status = Parsing_status::CODE_GENERATED;
+    };
+
 
 };
 
