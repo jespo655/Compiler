ffaad1229d4a74a5b2afe40598c740bb90d33298
jespo655 <jespo655@student.liu.se>
2018-09-09 11:16:13 +0200

    assert macro now exits cleanly instead of throwing exception; reformatted asserts using better macro conventions

--- Src/utilities/assert.h
+++ Src/utilities/assert.h
@@ -10,38 +10,34 @@
 #endif
 
 #ifdef DEBUG
-#	define GET_ASSERT_MACRO(_1,_2,ASSERT,...) ASSERT
-#	define ASSERT(...) GET_ASSERT_MACRO(__VA_ARGS__,ASSERT2,ASSERT1)(__VA_ARGS__)
+#	define _GET_ASSERT_MACRO(_1,_2,ASSERT,...) ASSERT
+#	define ASSERT(...) _GET_ASSERT_MACRO(__VA_ARGS__,_ASSERT2,_ASSERT1)(__VA_ARGS__)
 #else
-#	define ASSERT(...)
+#	define ASSERT(...) void(0)
 #endif
-
 
 #ifdef DEBUG
-#	define ASSERT1(b) if (!(b))                                                         \
-    {                                                                                   \
-        std::ostringstream oss;                                                         \
-        oss << __FILE__ << ":" << __LINE__ << ": Assert failed: (" << #b << ")";        \
-        std::cerr << oss.str();                                                         \
-        throw std::runtime_error(oss.str());                                            \
-    }
-#else
-#	define ASSERT1(b)
+#	define _ASSERT1(b) if (!(b))                                                        \
+    do {                                                                                \
+        std::ostringstream _assert_oss;                                                 \
+        _assert_oss << __FILE__ << ":" << __LINE__                                      \
+            << ": Assert failed: (" << #b << ")";                                       \
+        std::cerr << _assert_oss.str() << std::endl;                                    \
+        exit(1);                                                                        \
+    } while(0)
 #endif
-
 
 #ifdef DEBUG
-#	define ASSERT2(b, msg) if (!(b))                                                    \
-    {                                                                                   \
-        std::ostringstream _assert_oss;                                                         \
-        _assert_oss << __FILE__ << ":" << __LINE__ << ": Assert failed: (" << #b << ")";        \
-        _assert_oss << ": " << msg;                                                             \
-        std::cerr << _assert_oss.str();                                                         \
-        throw std::runtime_error(_assert_oss.str());                                            \
-    }
-#else
-#	define ASSERT2(b, msg)
+#	define _ASSERT2(b, msg) if (!(b))                                                   \
+    do {                                                                                \
+        std::ostringstream _assert_oss;                                                 \
+        _assert_oss << __FILE__ << ":" << __LINE__                                      \
+            << ": Assert failed: (" << #b << ")";                                       \
+        _assert_oss << ": " << msg;                                                     \
+        std::cerr << _assert_oss.str() << std::endl;                                    \
+        exit(1);                                                                        \
+    } while(0)
 #endif
 
-
 #endif
+
